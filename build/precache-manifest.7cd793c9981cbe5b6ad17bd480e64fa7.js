826bf6b21557cab33c77df6f637b7276566aae02
tintti68 <tintti1968@gmail.com>
2019-06-22 13:39:35 +0300

    ui

---
+++ build/precache-manifest.7cd793c9981cbe5b6ad17bd480e64fa7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06d4b0ea3b573e14d612eaf6dd88ee59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85df8bf55f662c91eee8",
+    "url": "/static/css/main.5f1adff5.chunk.css"
+  },
+  {
+    "revision": "c26d796be8d598825687",
+    "url": "/static/js/2.6bc084a2.chunk.js"
+  },
+  {
+    "revision": "85df8bf55f662c91eee8",
+    "url": "/static/js/main.26d1c455.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
