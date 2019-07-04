08ca8e83c210ba7996168e67636b44d929868905
Pekka <tintti1968@gmail.com>
2019-07-04 20:22:29 +0300

    korjaus

---
+++ build/precache-manifest.bce583b1b352108ae1363d46a7eacdc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bcb282b2be23929130bedeb37055e02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f211299c79186aab0500",
+    "url": "/static/css/main.5f1adff5.chunk.css"
+  },
+  {
+    "revision": "ab19420db37552f96701",
+    "url": "/static/js/2.97aa6aa9.chunk.js"
+  },
+  {
+    "revision": "f211299c79186aab0500",
+    "url": "/static/js/main.2d3a6bb3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
